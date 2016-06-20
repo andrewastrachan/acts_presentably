8217b775b92b5bc9e051f5ed6c0eb0882c670c20
Andrew Strachan <andrewastrachan@outlook.com>
2016-06-20 12:34:29 -0700

    rm byebug ref

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,4 @@
 require 'acts_presentably'
-require 'byebug'
 
 ActiveRecord::Base.establish_connection(:adapter => "sqlite3",
                                         :database => File.dirname(__FILE__) + "/acts_presentably.sqlite3")
